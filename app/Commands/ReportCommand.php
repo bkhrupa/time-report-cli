99c44add95facc70cdcaf07e15dd27e705fac840
Bohdan Khrupa <bohdan.khrupa@lemberg.co.uk>
2018-11-01 16:00:28 +0200

    Styling console output. Started sending progress.

--- app/Commands/ReportCommand.php
+++ app/Commands/ReportCommand.php
@@ -5,15 +5,21 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\File;
 use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class ReportCommand extends Command
 {
+
+    const ACTION_CALC = 'calc';
+    const ACTION_SEND = 'send';
+
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'report';
+    protected $name = 'report';
 
     /**
      * The description of the command.
@@ -29,6 +35,24 @@ class ReportCommand extends Command
      */
     public function handle()
     {
+        $isNeedToSend = false;
+        // Command Experiments
+//        if (!$this->hasOption('force') || !$this->option('force')) {
+//            $this->error(' already exists!');
+//
+//            return false;
+//        }
+
+        if ($this->hasArgument('action') && $this->argument('action') == self::ACTION_SEND) {
+            $isNeedToSend = true;
+        }
+
+        $this->processTimeSessions($isNeedToSend);
+
+    }
+
+    protected function processTimeSessions(bool $isNeedToSend = false)
+    {
         $totalMinutes = 0;
 
         foreach (File::allFiles(base_path('reports')) as $file) {
@@ -36,9 +60,17 @@ class ReportCommand extends Command
             $project = str_replace('.csv', '', $file->getFilename());
             $projectTotalMinutes = 0;
 
-            $this->comment('Project: ' . $project . ' (' . $system . ')');
+            $this->line('');
+            $this->line('Project: <comment>' . $project . '</comment> (<bg=default;fg=blue>' . $system . '</>)');
 
-            foreach (file($file->getPathname()) as $line) {
+            $rows = file($file->getPathname());
+
+            if ($isNeedToSend) {
+                $this->info('Starting send for project ' . $project . '. Tickets count ' . count($rows));
+                $this->output->progressStart(count($rows));
+            }
+
+            foreach ($rows as $line) {
                 if (!empty($line)) {
                     $row = explode(';', $line);
 
@@ -47,16 +79,26 @@ class ReportCommand extends Command
                     }, $row);
                     list($minutes, $ticket, $message) = $row;
 
-                    $this->line($minutes . "m\t" . $ticket . "\t" . $message);
+                    if (!$isNeedToSend) {
+                        $this->line($minutes . "m\t" . $ticket . "\t" . $message);
+                    }
                     $totalMinutes = $totalMinutes + (integer)$minutes;
                     $projectTotalMinutes = $projectTotalMinutes + (integer)$minutes;
+
+                    if ($isNeedToSend) {
+                        $this->output->progressAdvance(1);
+                        sleep(1);
+                    }
                 }
             }
 
-            $this->info('Total by "' . $project . '": ' . $this->minutesToHourString($projectTotalMinutes));
-            $this->line('');
+            if (!$isNeedToSend) {
+                $this->line('');
+                $this->info('Total by "' . $project . '": ' . $this->minutesToHourString($projectTotalMinutes));
+            }
         }
 
+        $this->line('');
         $this->alert('Total: ' . $this->minutesToHourString($totalMinutes) . ' (' . $totalMinutes . ') minutes');
     }
 
@@ -77,5 +119,43 @@ class ReportCommand extends Command
         $minutes = $int % 60;
 
         return $hours . 'h ' . $minutes . 'm';
+    }
+
+    protected function getActionInput()
+    {
+        return trim($this->argument('action'));
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['action', InputArgument::OPTIONAL, 'The action.', 'calc'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return
+            [
+                ['force', 'f', InputOption::VALUE_NONE, 'Force.'],
+
+                [
+                    'command',
+                    null,
+                    InputOption::VALUE_OPTIONAL,
+                    'The terminal command that should be assigned.',
+                    'command:name'
+                ],
+            ];
     }
 }
