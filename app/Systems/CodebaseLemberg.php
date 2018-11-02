9d8e5e419b64013b84735d2b686ba683ca864af2
Bogdan Khrupa <lizard.freddi@gmail.com>
2018-11-02 11:20:57 +0200

    small refactoring

--- app/Systems/CodebaseLemberg.php
+++ app/Systems/CodebaseLemberg.php
@@ -6,7 +6,19 @@ use GuzzleHttp\TransferStats;
 
 class CodebaseLemberg
 {
-    public function __construct($apiUrl, $domain, $userLogin, $apiToken)
+    protected $apiUrl;
+    protected $domain;
+    protected $userLogin;
+    protected $apiToken;
+
+    /**
+     * CodebaseLemberg constructor.
+     * @param string $apiUrl
+     * @param string $domain
+     * @param string $userLogin
+     * @param string $apiToken
+     */
+    public function __construct(string $apiUrl, string $domain, string $userLogin, string $apiToken)
     {
         $this->apiUrl = $apiUrl;
         $this->domain = $domain;
@@ -53,5 +65,45 @@ class CodebaseLemberg
         dump($url);
 
         dump(json_decode($res->getBody()));
+
+        //        // Update ticket
+//        $project = 'lemberg-hub';
+//        $ticketId = 2;
+//
+//        $client = new Client();
+//
+//        try {
+//            $res = $client->request(
+//                'POST',
+//                $apiUrl . '/' . $project . '/tickets/' . $ticketId . '/notes',
+//                [
+//                    'auth' => [$user, $password],
+//                    'headers' => [
+//                        'Content-Type' => 'application/json',
+//                        'Accept' => 'application/json',
+//                    ],
+//                    'json' => [
+//                        'ticket_note' => [
+//                            'content' => 'asd',
+//                        ]
+//                    ],
+//                    'on_stats' => function (TransferStats $stats) use (&$url) {
+//                        $url = $stats->getEffectiveUri();
+//                    }
+//                ]
+//            );
+//
+//
+//            dump($res->getStatusCode());
+//            dump($url);
+//
+//            dump(json_decode($res->getBody()));
+//
+//        }
+//        catch (ClientException $e) {
+//            $this->error($e->getMessage());
+//            dump(get_class($e));
+//            dump((string)$e->getResponse()->getBody());
+//        }
     }
 }
