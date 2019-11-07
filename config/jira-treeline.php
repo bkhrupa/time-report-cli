64f355c208e52d1d5d95d0d467353f36727ed0c8
Bohdan Khrupa <bohdan.khrupa@lemberg.co.uk>
2019-11-07 11:58:43 +0200

    typo fix.

--- config/jira-treeline.php
+++ config/jira-treeline.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'url' => env('JIRA_TREELINE_URL', 'https://treeline.atlassian.net'),
-    'user' => env('JIRA_TREELINE_USER', 'foo@treelineinteractive.com'),
+    'url' => env('JIRA_TREELINE_URL', 'https://atlassian.net'),
+    'user' => env('JIRA_TREELINE_USER', 'foo@gmail.com'),
     'password' => env('JIRA_TREELINE_PASSWORD', 'secret'),
-    'cookie_file' => storage_path('jira-cookie.txt'),
+    'cookie_file' => storage_path('jira-treeline-cookie.txt'),
 ];
